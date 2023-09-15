4045d3f6d463a2a5e33eff71a2ba2bdca68a7446
- <.@.com>
2023-09-15 18:31:11 -0300

    Modificando o arquivo index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
-var x = "<h1>Teste Cleisson</h1><p>teste 2</p>"
-
 function Home ()
 {
-	return x;
+	return <h1>Eu ia tentar colocar uma foto nossa, mas eu ainda não consegui, mas essa é a minha página oficial do curso kkkkkk, tudo o que eu fizer vai aparecer aqui amor, TE AMO MUITAOOOOO</h1>;
 }
 
 export default Home;
